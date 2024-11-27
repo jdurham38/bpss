fe5d400713e3c2baea17e619be3c1c1f3f108d6d
Joshua Durham <jdurham38@gmail.com>
2024-11-27 18:33:50 -0500

    full site style enhancements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/favicon.png" sizes="any" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
